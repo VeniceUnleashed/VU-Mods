f0f7170a069ef22fe7348b7624eefcb09f74e40a
Mats Bakken <powback@gmail.com>
2020-02-25 18:44:59 +0100

    Update EmitterTweaks

--- EmitterTweaks/ext/Client/__init__.lua
+++ EmitterTweaks/ext/Client/__init__.lua
@@ -1,40 +1,56 @@
-class 'EmitterTweaksClient'
-
-
-function EmitterTweaksClient:__init()
-	print("Initializing EmitterTweaksClient")
-	self:RegisterVars()
-	self:RegisterEvents()
-end
-
-
-function EmitterTweaksClient:RegisterVars()
-	--self.m_this = that
-end
-
-
-function EmitterTweaksClient:RegisterEvents()
-	self.m_ReadInstanceEvent = Events:Subscribe('Partition:ReadInstance', self, self.ReadInstance)
-end
-
-
-function EmitterTweaksClient:ReadInstance(p_Instance,p_PartitionGuid, p_Guid)
-	if p_Instance == nil then
+Events:Subscribe('Partition:Loaded', function(partition)
+	if partition == nil then
 		return
 	end
-	
-	if p_Instance.typeName == "EmitterTemplateData" then
-		local s_Instance = EmitterTemplateData(p_Instance)
 
+	local instances = partition.instances
+	for _, instance in pairs(instances) do
+		if instance ~= nil then
+			if instance:Is("EmitterTemplateData") then
+				local emitterTemplate = EmitterTemplateData(instance)
+
+
+				-- Tweak smoke and dust to last longer
+				if(string.find(emitterTemplate.name:lower(), "smoke") or string.find(emitterTemplate.name:lower(), "dust")) then
+					emitterTemplate:MakeWritable()
+					if (emitterTemplate.emissive or emitterTemplate.actAsPointLight or emitterTemplate.repeatParticleSpawning or emitterTemplate.opaque) == false then
+						if(emitterTemplate.rootProcessor:Is("UpdateAgeData")) then
+							local rootProcessor = UpdateAgeData(emitterTemplate.rootProcessor)
+							rootProcessor:MakeWritable()
+							rootProcessor.lifetime = rootProcessor.lifetime * 3
+
+							emitterTemplate.lifetime = emitterTemplate.lifetime * 3
+							emitterTemplate.maxCount = emitterTemplate.maxCount * 3
+							emitterTemplate.timeScale = emitterTemplate.timeScale / 2
+						end
+					end
+
+				-- Make muzzleflashes light up
+				elseif(string.find(emitterTemplate.name:lower(), "muzz")) then
+					emitterTemplate:MakeWritable()
+					emitterTemplate.actAsPointLight = true
+					if(emitterTemplate.pointLightColor == Vec3(1,1,1)) then
+						emitterTemplate.pointLightColor = Vec3(1,0.25,0)
+					end
+
+				-- Make bullets light up
+				elseif(string.find(emitterTemplate.name:lower(), "tracer")) then
+					emitterTemplate:MakeWritable()
+					emitterTemplate.actAsPointLight = true
+					if(emitterTemplate.pointLightColor == Vec3(1,1,1)) then
+						emitterTemplate.pointLightColor = Vec3(1,0.25,0) -- Change this to something suitable?
+					end
+
+				-- Make sparks light up
+				elseif(string.find(emitterTemplate.name:lower(), "spark")) then
+					emitterTemplate:MakeWritable()
+					emitterTemplate.actAsPointLight = true--emitterTemplate.lifetime * 3
+					if(emitterTemplate.pointLightColor == Vec3(1,1,1)) then
+						emitterTemplate.pointLightColor = Vec3(1,0.25,0) -- Change this to something suitable?
+					end
+				end
+			end
+		end
 	end
-	
-	
-	if p_Guid == Guid("Some-Guid", "D") then
-		local s_Instance = SomeClass(p_Instance)
-		s_Instance.someThing = true
-	end
-end
-
-
-g_EmitterTweaksClient = EmitterTweaksClient()
+end)
 
