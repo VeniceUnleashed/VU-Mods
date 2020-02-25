1f0e81d38a1c286602f92a57d9e55445fb258d93
Mats Bakken <powback@gmail.com>
2020-02-25 16:14:17 +0100

    Update NoFlightCeiling

--- NoFlightCeiling/ext/Server/__init__.lua
+++ NoFlightCeiling/ext/Server/__init__.lua
@@ -1,59 +1,17 @@
-class 'NoFlightCeilingServer'
-
-local SharedPatches = require '__shared/patches'
-local Inspect = require('inspect')
-
-function NoFlightCeilingServer:__init()
-	self:InitComponents()
-	self:RegisterEvents()
-	self:InstallHooks()
-end
-
-function NoFlightCeilingServer:RegisterEvents()
-	self.m_LoadedEvent = Events:Subscribe('ExtensionLoaded', self, self.Loaded)
-	self.m_EngineMessageEvent = Events:Subscribe('Engine:Message', self, self.OnEngineMessage)
-end
-
-function NoFlightCeilingServer:OnEngineMessage(p_Message)
-
-	if p_Message == nil then
+Events:Subscribe('Partition:Loaded', function(partition)
+	if partition == nil then
 		return
 	end
-	if p_Message.type == MessageType.ServerEntityOnDamageMessage then
-		print("ServerEntityOnDamageMessage")
+
+	local instances = partition.instances
+	for _, instance in pairs(instances) do
+		if instance ~= nil then
+			if instance:Is("LevelData") then
+				local instance = LevelData(instance)
+				instance:MakeWritable()
+				instance.maxVehicleHeight = 9999999
+			end
+		end
 	end
-	if p_Message.type == MessageType.ServerSoldierDamagedMessage then
-		print("ServerSoldierDamagedMessage")
-	end
-	if p_Message.type == MessageType.ServerSoldierSoldierDamageMessage then
-		print("ServerSoldierSoldierDamageMessage")
-	end
-	if p_Message.type == MessageType.ServerCollisionProjectileImpactMessage then
-		print("ServerCollisionProjectileImpactMessage")
-	end
+end)
 
-
-
-
-
-end
-
-
-
-
-function NoFlightCeilingServer:InstallHooks()
-
-end
-
-function NoFlightCeilingServer:InitComponents()
-	self.m_SharedPatches = SharedPatches()
-end
-
-function NoFlightCeilingServer:Loaded()
-	self.m_SharedPatches:OnLoaded()
-end
-
-
-
-
-g_NoFlightCeilingServer = NoFlightCeilingServer()
