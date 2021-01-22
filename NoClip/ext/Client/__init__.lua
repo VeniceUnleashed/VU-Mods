dd3cb27bbe5061f0b4f840173e9061fc60a0a43d
Ronan Munro <ronan18@live.nl>
2021-01-22 19:33:14 +0100

    fixed stuttering added speed multiplier added left right started on up down, does not work yet...

--- NoClip/ext/Client/__init__.lua
+++ NoClip/ext/Client/__init__.lua
@@ -37,8 +37,9 @@ function NoClipClient:Toggle()
 
 end
 
+
 function NoClipClient:OnUpdate(p_Delta, p_SimulationDelta)
-	
+
 	self.m_LastUpdated = self.m_LastUpdated + p_Delta
 
 	local s_NoclipKey = InputManager:WentKeyDown(InputDeviceKeys.IDK_V)
@@ -57,56 +58,75 @@ function NoClipClient:OnUpdate(p_Delta, p_SimulationDelta)
 
 	local s_FBMove = 0.0
 	local s_LRMove = 0.0
+	local s_UDMove = 0.0
+	local s_Multiplier = 1.0
+
+
+	local s_MouseWheel = InputManager:GetLevel(InputConceptIdentifiers.ConceptFreeCameraSwitchSpeed)
+	if s_MouseWheel == nil then
+		print("s_MouseWheel is nil")
+		return
+	else
+		s_Multiplier = s_Multiplier + (s_MouseWheel * 1)
+	end
+
+
+
+	if InputManager:IsKeyDown(InputDeviceKeys.IDK_Q) then
+		s_UDMove = 1.0 * s_Multiplier
+	elseif InputManager:IsKeyDown(InputDeviceKeys.IDK_E) then
+		s_UDMove = -1.0 * s_Multiplier
+	end
 
 	if InputManager:IsKeyDown(InputDeviceKeys.IDK_W) then
-		s_FBMove = 1.0
+	s_FBMove = 1.0 * s_Multiplier
 	elseif InputManager:IsKeyDown(InputDeviceKeys.IDK_S) then
-		s_FBMove = -1.0
+	s_FBMove = -1.0 * s_Multiplier
 	end
-		
+
 	if InputManager:IsKeyDown(InputDeviceKeys.IDK_A) then
-		s_LRMove = 1.0
+	s_LRMove = 1.0 * s_Multiplier
 	elseif InputManager:IsKeyDown(InputDeviceKeys.IDK_D) then
-		s_LRMove = -1.0
+	s_LRMove = -1.0 * s_Multiplier
 	end
 
-	self:UpdateVelocity(s_FBMove, s_LRMove)
+	self:UpdateVelocity(s_FBMove, s_LRMove, s_UDMove)
 
-end
-
-
-
-function NoClipClient:UpdateVelocity(p_FBMove, p_LRMove)
-	if(self.m_LastUpdated < 0.1) then 
-		return
 	end
-	if p_FBMove == 0 and p_LRMove == 0 then
-		return
+
+
+
+	function NoClipClient:UpdateVelocity(p_FBMove, p_LRMove, p_UDMove)
+	if(self.m_LastUpdated < 0.01) then
+	return
+	end
+	if p_FBMove == 0 and p_LRMove == 0 and p_LRMove == 0 then
+	return
 	end
 
 	local s_Player = PlayerManager:GetLocalPlayer()
 	if s_Player == nil then
-		return
+	return
 	end
 
 	local s_Soldier = s_Player.soldier
 	if s_Soldier == nil then
-		return
+	return
 	end
 
 	local s_SoldierPhysics = s_Soldier.physicsEntityBase
 	if s_SoldierPhysics == nil then
-		return
+	return
 	end
 
 	print("Calcing velocity")
 
 	local s_CameraTransform = ClientUtils:GetCameraTransform()
-	
+
 	print("Got camera " .. tostring(s_CameraTransform.forward.x))
 	print("p_FBMove" .. tostring(p_FBMove))
 	print("p_LRMove" .. tostring(p_LRMove))
-	local s_Velocity = s_CameraTransform.forward*p_FBMove
+	local s_Velocity = s_CameraTransform.forward*p_FBMove + s_CameraTransform.left*p_LRMove + s_CameraTransform.up*p_UDMove
 
 	print("after camera " .. tostring(s_Velocity.x))
 	-- calc velocity
@@ -116,11 +136,13 @@ function NoClipClient:UpdateVelocity(p_FBMove, p_LRMove)
 	-- client prediction
 	--s_SoldierPhysics.linearVelocity = s_Velocity
 
+	s_Player.soldier:SetPosition( s_Player.soldier.physicsEntityBase.position - s_Velocity )
+
 
 	NetEvents:SendLocal('NoClip:SetVelocity', s_Velocity)
 
 	self.m_LastUpdated = 0
-end
+	end
 
 
-g_NoClipClient = NoClipClient()
+	g_NoClipClient = NoClipClient()
